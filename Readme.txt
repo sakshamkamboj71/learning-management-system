6e1b6fc5c7eb73a00f7493340eb606376f56e53a
Saksham Kamboj <sakshamkamboj71@gmail.com>
2024-02-16 12:35:56 +0530

    change readme

--- Readme.txt
+++ Readme.txt
@@ -5,3 +5,20 @@ npm install react-icons react-router-dom
 //Inside the server folder
 
 npm install axios bcrypt mongoose
+
+
+
+//Git push and pull commands
+
+//Clone 
+git clone https://github.com/sakshamkamboj71/learning-management-system.git
+
+//Pull
+git pull https://github.com/sakshamkamboj71/learning-management-system.git
+
+//Push
+git add *any file*
+git commmit -m "random message"
+git status
+git remote add origin https://github.com/sakshamkamboj71/learning-management-system.git
+git push -u origin main
--- client/src/Default.js
+++ client/src/Default.js
@@ -13,7 +13,7 @@ const Default = () => {
     >
       <div className="block sm:flex sm:flex-row w-full h-2/3 sm:h-1/2 xl:h-full xl:w-2/3">
         <div className="w-full sm:w-1/2 h-1/2 sm:h-full p-2 sm:p-4 xl:p-10 cursor-pointer">
-          <div className="flex sm:block items-center justify-between px-4 bg-black w-full h-full text-gray-400 hover:text-[#d872fc] hover-scale-[1.03] sm:hover:scale-[1.05] hover:border-[#d872fc] hover:border-2 rounded-lg border-white border-2">
+          <div className="flex sm:block items-center justify-between px-4 bg-black w-full h-full text-gray-400 hover:text-[#d872fc] hover-scale-[1.03] sm:hover:scale-[1.05] hover:border-[#d872fc] hover:border-2 rounded-lg border-gray-400 border-2">
             <div>
               <h1 className="text-xl sm:text-3xl lg:text-4xl text-center font-semibold sm:pt-4">
                 Sign in as
